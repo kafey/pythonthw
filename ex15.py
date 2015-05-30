5bec2cdeb932a2d752e1c8cefb2bddc8d0fe896b
kafey <febri@saklik.com>
2015-05-30 11:20:27 +0700

    setelah comment

--- ex15.py
+++ ex15.py
@@ -1,9 +1,10 @@
+# request function argv from library sys
 from sys import argv
-
+# input in cli when running the code $pyhton ex15.py ex15_sample.txt
 script , filename = argv
-
+# open file ex15_sample.txt
 txt = open(filename)
-
+# print the file
 print "Here's your life %r" % filename
 print txt.read()
 
